8b84e92a3268e327c20888c59cfd5f80b74be486
Leon <leon.kouz@hotmail.com>
2017-12-19 09:51:09 +1100

    Added comments to SettingsActivity

--- Bittrex/SettingsActivity.cs
+++ Bittrex/SettingsActivity.cs
@@ -29,28 +29,36 @@ namespace Bittrex
         {
             base.OnCreate(savedInstanceState);
 
+            //Set the view to Settings page
             SetContentView(Resource.Layout.Settings);
 
+            //Get the buttons that allow you to set the keys
             var apiKeyButton = (Button)FindViewById(Resource.Id.settings_apiKeyButton);
             var secretKeyButton = (Button)FindViewById(Resource.Id.settings_secretKeyButton);
 
+            //Subscribe to click events
             apiKeyButton.Click += ApiKeyButton_Click;
             secretKeyButton.Click += SecretKeyButton_Click;
 
+            //Get the TextViews that shows the users keys
             apiKeyText = (TextView)FindViewById(Resource.Id.settings_apiKeyTextView);
             secretKeyText = (TextView)FindViewById(Resource.Id.settings_secretKeyTextView);
 
+            //Set the TextViews to the currently saved keys
             apiKeyText.Text = LoginData.APIKey;
             secretKeyText.Text = LoginData.SecretKey;
         }
 
         private void SecretKeyButton_Click(object sender, EventArgs e)
         {
+            //Inflate the dialog layout that should appear when a button is pressed
             LayoutInflater layoutInfalter = LayoutInflater.From(this);
             View mView = layoutInfalter.Inflate(Resource.Layout.Settings_inputlayout, null);
 
+            //Get the text input that appears on the dialog 
             var inputText = (TextView)mView.FindViewById(Resource.Id.settings_inputText);
 
+            
             AlertDialog.Builder ad = new AlertDialog.Builder(this);
             ad.SetTitle("Enter your new Secret Key");
             ad.SetCancelable(false).SetPositiveButton("Okay", delegate
@@ -84,9 +92,11 @@ namespace Bittrex
 
         private void ApiKeyButton_Click(object sender, EventArgs e)
         {
+            //Inflate the dialog layout that should appear when a button is pressed
             LayoutInflater layoutInfalter = LayoutInflater.From(this);
             View mView = layoutInfalter.Inflate(Resource.Layout.Settings_inputlayout, null);
 
+            //Get the text input that appears on the dialog 
             var inputText = (TextView)mView.FindViewById(Resource.Id.settings_inputText);
 
             AlertDialog.Builder ad = new AlertDialog.Builder(this);
